df90770a5df265df569db130ae3342ea089f76df
archana01072003 <85079777+archana01072003@users.noreply.github.com>
2022-10-23 00:11:32 +0530

    Create isPrime.cpp

---
+++ isPrime.cpp
@@ -0,0 +1,27 @@
+//program to check number is prime or not
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+	// Corner case
+	if (n <= 1)
+		return false;
+
+	// Check from 2 to n-1
+	for (int i = 2; i < n; i++)
+		if (n % i == 0)
+			return false;
+
+	return true;
+}
+int main()
+{
+	int n;
+    cin >> n;
+    if(isPrime(n))
+        cout<<"Prime"<<endl;
+    else
+        cout<<"Not Prime"<<endl;
+	return 0;
+}
